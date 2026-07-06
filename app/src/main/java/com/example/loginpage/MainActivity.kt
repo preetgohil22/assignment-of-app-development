967facc910f1c2d756045d73c0f0980d7a897e7d
Preet Gohil <preetgohil22@gmail.com>
2026-07-06 13:03:29 +0530

    project

---
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.loginpage
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val bnt = findViewById<Button>(R.id.btn1)
+        bnt.setOnClickListener {
+            Toast.makeText(this, "success login", Toast.LENGTH_SHORT).show()
+            val intent = Intent(applicationContext, implicitAndwebview::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/loginpage/implicitAndwebview.kt
@@ -0,0 +1,36 @@
+package com.example.loginpage
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class implicitAndwebview : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_implicit_andexplicit)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val webview1= findViewById<WebView>(R.id.webview1)
+        webview1.apply {
+            settings.javaScriptEnabled=true
+            settings.safeBrowsingEnabled=true
+            webview1.loadUrl("https://www.amazon.in/")
+        }
+
+
+    }
+
+}
